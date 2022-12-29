b1755f4dbf27ed2b87bd13d4a5642fc460ba7b1a
Yago <yago@lumanet.com.br>
2022-12-29 10:15:52 -0300

    voltando ao normal

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from ext import database, configuration
+import views
+
+app = Flask(__name__)
+
+configuration.init_app(app)
+database.init_app(app)
+views.init_app(app)
+
+if __name__ == "__main__":
+    app.run()
