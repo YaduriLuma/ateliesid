5f11a85befed4e41334bb08ac30488c40d0ad835
Yago <yago@lumanet.com.br>
2022-12-29 11:09:19 -0300

    make it better

--- ext/configuration.py
+++ ext/configuration.py
@@ -1,9 +1,7 @@
-from dynaconf import FlaskDynaconf
 from flask_bootstrap import Bootstrap
 
 
 def init_app(app):
     app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
-    FlaskDynaconf(app)
     Bootstrap(app)
     app.config['TITLE'] = 'Social'
