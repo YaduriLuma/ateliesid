4d01bf378e2f7d692479dd9111c1a0893e8f421e
Yago <yago@lumanet.com.br>
2022-12-29 09:41:42 -0300

    estruturando 2.0

--- atelie/ext/configuration.py
+++ atelie/ext/configuration.py
@@ -1,7 +1,9 @@
+from dynaconf import FlaskDynaconf
 from flask_bootstrap import Bootstrap
 
 
 def init_app(app):
     app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+    FlaskDynaconf(app)
     Bootstrap(app)
     app.config['TITLE'] = 'Social'
