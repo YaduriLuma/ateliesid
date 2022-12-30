e358537f76969bf71d1c66324e0ac4207493180c
Yago <yago@lumanet.com.br>
2022-12-30 12:53:48 -0300

    make it better

--- ext/database.py
+++ ext/database.py
@@ -4,6 +4,7 @@ db = SQLAlchemy()
 
 
 def init_app(app):
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mssql+pymssql://sa:@BRUNO-PC\SQLEXPRESS/ATELIE_DB'
+    """app.config['SQLALCHEMY_DATABASE_URI'] = 'mssql+pymssql://sa:@BRUNO-PC\SQLEXPRESS/ATELIE_DB'"""
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://sa:NXXq2BBdSpUtSFit4VzRF10XLpCN0jsm@dpg-cenghg82i3molpkn2dig-a.oregon-postgres.render.com/ateliedb'
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.init_app(app)
